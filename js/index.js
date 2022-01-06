25726bc066d6a0898b46399cb03a00acc6c1e53e
psabino84 <sabino.pablo@gmail.com>
2022-01-06 16:30:14 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/* Autorización de ingreso *//*
+let edad = prompt ("Por favor ingresa tu edad");
+    if (edad >= 18) {
+        alert ("Bienvenido");
+    } else {
+        alert ("Debes ser mayor de 18 para poder estar aqui");
+    }
